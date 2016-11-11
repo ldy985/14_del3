2595ba651026f8826655e25e40cceff77ca49deb
Unknown <razzer58@gmail.com>
2016-11-11 16:13:52 +0100

    Fixed Laborcamp.class

--- src/Game/LaborCamp.java
+++ src/Game/LaborCamp.java
@@ -2,31 +2,63 @@ package Game;
 
 public class LaborCamp extends Ownable {
 
-    private int baseRent = Shaker.getSum()*100;
+    private int baseRent;
+    private int no;
+    private int price;
+    private String name;
+    private boolean buyfield;
+    private boolean ableToBuy;
 
-    private LaborCamp(int no, String name, int price){
 
-        this.no=no;
-        this.name=name;
-        this.price=price;
+    public LaborCamp(int no, String name, int price) {
+
+        super(no, price, name);
+
+        this.no = no;
+        this.name = name;
+        this.price = price;
+    }
+
+    public void landOnField(Player player) {
+
+        // If the player doesn't own the field
+        if (getOwner() != player) {
+
+            // If no-one owns the field
+            if (getOwner() == null) {
+
+                // Checks if the player has enough funds in their account
+                if (price <= player.getAccount().getBalance()) {
+
+                    ableToBuy = true;
+                }
+
+                // Checks whether the player wants to buy
+                if (buyfield && ableToBuy) {
+
+                    player.getAccount().addBalance(price);
+                    setOwner(player);
+                }
+                //If another player owns the field
+            } else {
+
+                player.getAccount().addBalance(getRent());
+            }
+
+        }
 
     }
 
-    private int getRent(Shaker){
+    @Override
+    public int getRent() {
+
+        // Receives the sum of the dice rolled
+        int sum = GameController.getShaker().getSum();
+
+        // Calculates the base rent
+        baseRent = sum * (-100);
 
         return baseRent;
-
     }
-
-    public landOnField{
-
-    }
-
-
-
-
-
-
-
 
 }
