ff0d282caa823ac9ca21ec7c06c9bb81b30428b7
Shadow <lasse@dyrsted.dk>
2016-11-11 16:20:48 +0100

    Add comments and header to Ownable

--- src/Game/Ownable.java
+++ src/Game/Ownable.java
@@ -2,24 +2,46 @@ package Game;
 
 
 /**
- * Created by Timothy Rasmussen on 10-11-2016.
+ * Shaker class, contains the Die objects
+ *
+ * Bugs: none known
+ *
+ * @author       Timothy Rasmussen
+ * @version      v.0.1
  */
 public abstract class Ownable extends Field {
 
     private int price;
     private Player owner;
 
+    /**
+     * The constructor of the Ownable type
+     * @param number The id of the Field
+     * @param price The price of the Field
+     * @param name The name of the Field
+     */
     public Ownable(int number, int price, String name) {
-        super(name, number);
+        super(number, name);
         this.price = price;
     }
 
+    /**
+     * Make sure all subclasses implement this method
+     */
     protected abstract int getRent();
 
+    /**
+     * Gets the owner of an ownable Field
+     * @return returns the owner.
+     */
     public Player getOwner() {
         return owner;
     }
 
+    /**
+     * Sets the owner of an ownable field
+     * @param owner The new owner
+     */
     public void setOwner(Player owner) {
         this.owner = owner;
     }
