1bcfc7790dd5ce19e10ad5d543a11a19c6778d78
Unknown <casperbodskov@live.dk>
2016-11-11 15:20:23 +0100

    Oops

--- src/Test/Account_Test.java
+++ src/Test/Account_Test.java
@@ -16,21 +16,4 @@ public class Account_Test {
 
 
 }
-    /*
-        // Sets the initial balance of the two players.
-        public Account(int initialBalance) {
-            balance = initialBalance;
-        }
-
-        // Adds/subtracts point to/from the balance by the given amount.
-        public boolean addBalance(int amount) {
-            balance = balance + amount;
-            return balance;
-        }
-
-        // Return the balance.
-        public int getBalance() {
-            return balance;
-        }
-
-    }
+   
