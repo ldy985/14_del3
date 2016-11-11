2bbd25319bd18b665936b79c7a731042c0eb8749
Unknown <casperbodskov@live.dk>
2016-11-11 14:10:17 +0100

    WIP. will fix dw

--- src/Test/Account_Test.java
+++ src/Test/Account_Test.java
@@ -1,4 +1,23 @@
 package Test;
 
 public class Account_Test {
-}
+
+        private int balance;
+
+        // Sets the initial balance of the two players.
+        public Account(int initialBalance) {
+            balance = initialBalance;
+        }
+
+        // Adds/subtracts point to/from the balance by the given amount.
+        public boolean addBalance(int amount) {
+            balance = balance + amount;
+            return balance;
+        }
+
+        // Return the balance.
+        public int getBalance() {
+            return balance;
+        }
+
+    }
