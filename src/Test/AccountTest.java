f3b746eefe9448dd54ac45b5030fd6e2f493a045
Unknown <tsra15@student.aau.dk>
2016-11-23 13:26:42 +0100

    AccountTestFix
    Fixed the AccountTest class

--- src/Test/AccountTest.java
+++ src/Test/AccountTest.java
@@ -1,5 +1,6 @@
 package Test;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -14,23 +15,30 @@ import static org.junit.Assert.assertEquals;
  */
 
 public class AccountTest {
-    //creates a account object
-    private final Game.Account account = new Game.Account(1000);
+
+    private Game.Account newAccount;
+
+    @Before
+    private void setUp() throws Exception {
+
+        newAccount = new Game.Account(1000);
+
+    }
 
     @Test
     public void addBalance() throws Exception {
 
-        account.addBalance(100);
+        newAccount.addBalance(100);
 
         //tests if the addBalance works
-        assertEquals(1100, account.getBalance());
+        assertEquals(1100, newAccount.getBalance());
     }
 
     @Test
     public void getBalance() throws Exception {
 
         //tests if the getBalance returns the right amount and if the starting balance is 1000 points
-        assertEquals(1000, account.getBalance());
+        assertEquals(1000, newAccount.getBalance());
     }
 
 
